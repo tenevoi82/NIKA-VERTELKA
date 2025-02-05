cd081a06b26e366a4ad35524a934e7254a5600b8
tenevoi82 <tenevoi82@gmail.com>
2025-02-06 01:16:06 +0200

    Добавил класс для надёжной посылки пакетов по софт порту (Пока не работает)

--- VERTELCA/atmega328p/src/atmega328.cpp
+++ VERTELCA/atmega328p/src/atmega328.cpp
@@ -1,6 +1,8 @@
 #include "Arduino.h"
 //Пример генерации меандра на таймере 2 , канале B (D3 на Arduino UNO)
 #include <GyverTimers.h>
+#include <SoftwareSerial.h>
+#include "net.hpp"
 void set_speed(float ob_min);
 void stop_engine();
 #define PIN_PAS 19
@@ -10,6 +12,15 @@ void stop_engine();
 
 uint32_t current_step = 0;
 float current_speed = 1;  // скорость гадусов в минуту
+
+// Создаём объект SoftwareSerial для ATmega328p на пинах 10 (RX) и 11 (TX)
+// Обычно на ATmega328p аппаратный Serial используется для отладки, поэтому здесь программный порт позволяет использовать другой канал для связи
+SoftwareSerial softSerial(0, 1);
+
+// Создаём глобальный объект протокола, передавая ему ранее созданный объект softSerial
+SerialProtocol protocol(softSerial);
+
+
 
 void stop_engine() {
 }
@@ -28,8 +39,8 @@ void setup() {
 
   Timer2.enableISR(CHANNEL_B);
   set_speed(current_speed);  //отключить когда настрою протокол обмена
-  Serial.begin(9600);
-  Serial.println("Start 44");
+  //Serial.begin(9600);
+  protocol.begin(9600); // Инициализируем SoftwareSerial для обмена данными на скорости 9600 бод
 }
 
 ISR(TIMER2_B) {
@@ -95,20 +106,22 @@ struct SerialData{
 #pragma pack(pop)
 
 
+
+
 void loop() {
 
   static unsigned long m = millis();
   if(millis()- m > 3000){
     m=millis();
-    Serial.println("Serial data:");
+    //Serial.println("Serial data:");
     SerialData d;
     d.Size = 3;
     d.data[0] = 1;
     d.data[1] = 2;
     d.data [2] = 3;
     d.crc = crc8_bytes((byte*)&d, sizeof(d) - 1);
-    Serial.write((byte*)&d,sizeof(d));
-    Serial.println("\r\nEND");
+    //Serial.write((byte*)&d,sizeof(d));
+    //Serial.println("\r\nEND");
   }
 
 }
--- VERTELCA/esp8266 wemos d1 mini/src/main.cpp
+++ VERTELCA/esp8266 wemos d1 mini/src/main.cpp
@@ -2,30 +2,24 @@
 
 #include <Arduino.h>
 #include <SPI.h>
-// #include <Wire.h>
 #include <RTClib.h>
-#include <SoftwareSerial.h>  // Подключение библиотеки Software Serial
-SoftwareSerial swSerial(D5, D6);  // RX, TX // Назначение задействованных дискретных каналов
+#include <SoftwareSerial.h> // Подключение библиотеки Software Serial
+#include <ESP8266WiFi.h>
+#include <GyverDBFile.h>
+#include <LittleFS.h>
+#include <SettingsGyver.h>
+#include "net2.hpp"
+
+GyverDBFile db(&LittleFS, "/settings.db");
+
+SettingsGyver sett("NIKA - Вертелка", &db);
+
+SoftwareSerial swSerial(D5, D6); // RX, TX // Назначение задействованных дискретных каналов
 
 #define WIFI_SSID "tenevoi"
 #define WIFI_PASS "dimadima"
 
-bool zeroPosition = false;
-//TwoWire tw;
 RTC_DS3231 rtc;
-
-#ifdef ESP8266
-#include <ESP8266WiFi.h>
-#else
-#include <WiFi.h>
-#endif
-
-#include <GyverDBFile.h>
-#include <LittleFS.h>
-GyverDBFile db(&LittleFS, "/settings.db");
-
-#include <SettingsGyver.h>
-SettingsGyver sett("NIKA - Вертелка", &db);
 
 DB_KEYS(
     kk,
@@ -38,7 +32,8 @@ DB_KEYS(
     rotation,
     softstop);
 
-struct Data {
+struct Data
+{
     DateTime rtcTime;
     uint32_t unixtime;
     float temp;
@@ -48,26 +43,31 @@ struct Data {
 Data data;
 
 int freq = 0;
-void build(sets::Builder& b) {
+void build(sets::Builder &b)
+{
     b.DateTime("Время на устр.", &data.unixtime);
-    b.LabelFloat("Температура",data.temp,2, sets::Colors::Blue);
-    if (b.Switch(kk::disableall, "Выключить")) {
+    b.LabelFloat("Температура", data.temp, 2, sets::Colors::Blue);
+    if (b.Switch(kk::disableall, "Выключить"))
+    {
         sett.reload();
     }
     (db[kk::disableall].toBool()) ? analogWrite(12, 0) : analogWrite(12, 1);
-    if (!db[kk::disableall].toBool()) {
+    if (!db[kk::disableall].toBool())
+    {
         {
             sets::Group g1(b, "Управление");
             b.Switch(kk::rotation, "Вращение против часовой");
-            if (b.Slider(kk::speed, "Скорость вращения", 0.1, 2, 0.1, " об/мин")) {
+            if (b.Slider(kk::speed, "Скорость вращения", 0.1, 2, 0.1, " об/мин"))
+            {
                 freq = 1000 * db[kk::speed].toFloat();
                 analogWriteFreq(freq);
             }
             if (b.Switch(kk::enabled, "Расписание вкл/выкл"))
-                b.reload();  // перезагрузить вебморду по клику на свитч
+                b.reload(); // перезагрузить вебморду по клику на свитч
         }
         {
-            if (db[kk::enabled]) {
+            if (db[kk::enabled])
+            {
                 sets::Group g2(b, "Расписание");
                 b.Time(kk::timeOn, "Время включения");
                 b.Time(kk::timeOff, "Время выключения");
@@ -78,55 +78,51 @@ void build(sets::Builder& b) {
     }
 }
 
-void update(sets::Updater& u) {
-}
+// void update(sets::Updater& u) {
+// }
 
-void setup() {
-    //delay(1000);
-    // TwoWire tw;
-    // tw.setClock(200000);
-    // tw.begin();
-    
-    
+// Создаём объект SoftwareSerial, назначая пины 5 для приема (RX) и 4 для передачи (TX)
+// SoftwareSerial softSerial(5, 4);
+
+// Создаём глобальный объект протокола, передавая ему объект softSerial
+SerialProtocol protocol(swSerial);
+
+void setup()
+{
+
+    // delay(1000);
+    //  TwoWire tw;
+    //  tw.setClock(200000);
+    //  tw.begin();
+
+    protocol.begin(9600); // Инициализируем SoftwareSerial для обмена данными на скорости 9600 бод
     Serial.begin(74880);
-        // initializing the rtc
-        //delay(1000);
-        digitalWrite(D1,HIGH);
-        delay(10);
-        digitalWrite(D1,LOW);
-        delay(10);
-        
-    while(!rtc.begin()) {
-        Serial.println("Couldn't find RTC!");
-        delay(1000);
-    }
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    swSerial.begin(9600);  // Инициализация программного последовательного порта
-    // pinMode(14, INPUT_PULLUP);
-    // analogWriteResolution(4);
-    // pinMode(12, OUTPUT);
-    // pinMode(13, OUTPUT);
+    // while (!rtc.begin())
+    // {
+    //     Serial.println("Couldn't find  RTC!");
+    //     delay(1000);
+    // }
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // swSerial.begin(9600);  // Инициализация программного последовательного порта
 
     // ======== WIFI ========
     // STA
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
     uint8_t tries = 20;
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         Serial.print(".");
-        if (!--tries) break;
+        if (!--tries)
+            break;
     }
     Serial.println();
     Serial.print("Connected: ");
     Serial.println(WiFi.localIP());
 
     // ======== DATABASE ========
-#ifdef ESP32
-    LittleFS.begin(true);
-#else
     LittleFS.begin();
-#endif
     db.begin();
     db.init(kk::disableall, false);
     db.init(kk::timeOn, 60);
@@ -138,7 +134,8 @@ void setup() {
     db.init(kk::softstop, true);
     db.dump(Serial);
     freq = 1000 * db[kk::speed].toFloat();
-    if (!db[kk::disableall].toBool()) analogWriteFreq(freq);
+    if (!db[kk::disableall].toBool())
+        analogWriteFreq(freq);
 
     // ======== SETTINGS ========
     sett.config.sliderTout = 1000;
@@ -146,87 +143,50 @@ void setup() {
     sett.config.updateTout = 500;
     sett.begin();
     sett.onBuild(build);
-    sett.onUpdate(update);
+    // sett.onUpdate(update);
 }
 
-void printTime() {
+void printTime()
+{
     static unsigned long m = millis();
-    if (millis() - m > 5000) {
-        data.rtcTime = rtc.now();
+    if (millis() - m > 5000)
+    {
+        //data.rtcTime = rtc.now();
         data.unixtime = data.rtcTime.unixtime();
         sett.reload();
         char timeText[128];
-        sprintf(timeText, "%02d:%02d:%02d %02d/%02d/%02d", data.rtcTime.hour(), data.rtcTime.minute(), data.rtcTime.second(),
-                data.rtcTime.day(), data.rtcTime.month(), data.rtcTime.year());
-        Serial.println(timeText);
-        Serial.println(data.unixtime);
-        Serial.println(rtc.getTemperature());
+        // sprintf(timeText, "%02d:%02d:%02d %02d/%02d/%02d", data.rtcTime.hour(), data.rtcTime.minute(), data.rtcTime.second(),
+        //         data.rtcTime.day(), data.rtcTime.month(), data.rtcTime.year());
+        // Serial.println(timeText);
+        // Serial.println(data.unixtime);
+        // Serial.println(rtc.getTemperature());
         m = millis();
     }
 }
 
-#pragma pack(push, 1)
-struct SerialData {
-    uint8_t magic = '$';
-    uint16_t Size;
-    uint16_t data[6] = {0, 0, 0, 0, 0, 0};
-    uint8_t crc;
-};
-#pragma pack(pop)
 
-// функция для расчёта crc
-uint8_t crc8_bytes(uint8_t* buffer, uint8_t size) {
-    byte crc = 0;
-    for (byte i = 0; i < size; i++) {
-        byte data = buffer[i];
-        for (int j = 8; j > 0; j--) {
-            crc = ((crc ^ data) & 1) ? (crc >> 1) ^ 0x8C : (crc >> 1);
-            data >>= 1;
-        }
-    }
-    return crc;
-};
+void loop()
+{
+    uint16_t d [] = {0, 0, 0, 0, 0, 0};
 
-int checkMB(byte* d, int s) {
-    for (int i = 0; i < s; i++) {
-        if (d[i] == '$')
-            return i;
-    }
-    return -1;
-}
 
-void loop() {
-    SerialData d;
 
-    data.unixtime = data.rtcTime.unixtime();
-    data.temp =rtc.getTemperature();
+    //data.unixtime = data.rtcTime.unixtime();
+    //data.temp = rtc.getTemperature();
     sett.tick();
-    printTime();
-    if (millis() % 1000 == 0) {
-        // char timeText[128];
-        //  int speed = 1000 * db[kk::speed].toFloat();
-        //  sprintf(timeText, "%f , frec = %d", db[kk::speed].toFloat(), speed);
-        //  analogWriteFreq(speed);
-        //  Serial.println(timeText);
+    static auto f = millis();
+    if(millis() - f  > 5000){
+        Serial.print("Отправляем днанные длинной ");
+        Serial.print(sizeof(d));
+        Serial.println(" байт....");
+        protocol.ackTimeout_ms = 3000;
+        protocol.sendPacketNonBlocking((uint8_t*)d,sizeof(d),true);
+        Serial.println("Отправленно.");
+        f = millis();
     }
-    byte buffer[128] = {
-        0,
-    };
-    int size;
-    size = Serial.available();
-    while (size > 0) {
-        Serial.read(buffer, size);
-        swSerial.write(buffer, size);
-        size = Serial.available();
-    }
-    size = swSerial.available();
-    while (size > 0) {
-        swSerial.read(buffer, size);
-        if (checkMB(buffer, size) != -1) {
-            Serial.print("Got MB on pozition");
-            Serial.println(checkMB(buffer, size));
-        }
-        Serial.write(buffer, size);
-        size = swSerial.available();
-    }
+
+    // Вызываем метод update() объекта протокола для асинхронной обработки входящих данных,
+    // проверки состояния ожидания подтверждения и управления отправкой/приёмом пакетов
+    protocol.update();
+    //printTime();
 }
